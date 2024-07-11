9492e9ffebd726cc5d8a86badce62210576e030a
burakarslan8 <burakarslan271@gmail.com>
2024-07-11 10:19:35 +0300

    Replace email notifier with a py script

--- hello.groovy
+++ hello.groovy
@@ -5,6 +5,7 @@ pipeline {
         // both recipient and sender email addresses must be verified in AWS SES
         string(name: 'recipientEmail', defaultValue: 'example@tokeninc.com')
         string(name: 'senderEmail', defaultValue: 'example@tokeninc.com')
+        booleanParam(name: 'IS_FAKE', defaultValue: 'false')
     }
 
     stages {
@@ -38,59 +39,46 @@ pipeline {
     // email notification
     post{
         success{
+            // TODO: the script should be in the workspace of the pipeline
+
             script{
                 echo 'Success'
                 // build time before post actions
                 Float buildTime = currentBuild.duration/1000
-                def subject = "Pipeline: ${JOB_NAME} - Build #${BUILD_NUMBER} - ${currentBuild.currentResult} | ${buildTime}s"
-                print subject
-                mail(
-                    to: "${recipientEmail}",
-                    subject: subject,
-                    body: "The build is successful",
-                    replyTo: "${senderEmail}",
-                    from: "${senderEmail}",
-                )
-                echo 'Email Sent!'
+                if (IS_FAKE == 'false'){
+
+                    sh '''
+                        chmod +x email_notifier.py
+                        python3 email_notifier.py ${senderEmail} ${recipientEmail} ${JOB_NAME} ${BUILD_NUMBER} ${currentBuild.currentResult} ${currentBuild.duration}
+                    '''
+                    echo 'Email sent!'
+                }
+                else {
+                    echo 'Email not sent because it is a fake build.'
+                }
                 buildTime = currentBuild.duration/1000
                 echo "Build time including post actions: ${buildTime}s"
             }
             
         }
-        unstable{
-            script{
-                echo 'Unstable'
-                // build time before post actions
-                Float buildTime = currentBuild.duration/1000
-                def subject = "Pipeline: ${JOB_NAME} - Build #${BUILD_NUMBER} - ${currentBuild.currentResult} | ${buildTime}s"
-                print subject
-                mail(
-                    to: "${recipientEmail}",
-                    subject: subject,
-                    body: "The build is unstable",
-                    replyTo: "${senderEmail}",
-                    from: "${senderEmail}",
-                )
-                echo 'Email Sent!'
-                buildTime = currentBuild.duration/1000
-                echo "Build time including post actions: ${buildTime}s"
-            }
-        }
         failure{
+            // TODO: the script should be in the workspace of the pipeline
+            
             script{
                 echo 'Failure'
                 // build time before post actions
                 Float buildTime = currentBuild.duration/1000
-                def subject = "Pipeline: ${JOB_NAME} - Build #${BUILD_NUMBER} - ${currentBuild.currentResult} | ${buildTime}s"
-                print subject
-                mail(
-                    to: "${recipientEmail}",
-                    subject: subject,
-                    body: "The build has failed",
-                    replyTo: "${senderEmail}",
-                    from: "${senderEmail}",
-                )
-                echo 'Email Sent!'
+                if (IS_FAKE == 'false'){
+
+                    sh '''
+                        chmod +x email_notifier.py
+                        python3 email_notifier.py ${senderEmail} ${recipientEmail} ${JOB_NAME} ${BUILD_NUMBER} ${currentBuild.currentResult} ${currentBuild.duration}
+                    '''
+                    echo 'Email sent!'
+                }
+                else {
+                    echo 'Email not sent because it is a fake build.'
+                }
                 buildTime = currentBuild.duration/1000
                 echo "Build time including post actions: ${buildTime}s"
             }
